09c95c829a084f236fe55f5eaf6bb28bb81a730d
melihorhanyilmaz <melih.yilmaz99@hotmail.com>
2023-06-09 14:30:01 +0200

    ı hope last

--- predict/prediction.py
+++ predict/prediction.py
@@ -1,7 +1,8 @@
 import pickle
 
-with open("models\model.sav", 'rb') as file:
+with open("models\\model.sav", 'rb') as file:
     loaded_model = pickle.load(file)
+    
 
 def predict(df):
     return loaded_model.predict(df)
